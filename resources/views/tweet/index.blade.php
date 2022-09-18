f6872983666b978fa60a7a29897bc48162786bb7
Satoshi-Tb <sulp91_fy@yahoo.co.jp>
2022-09-18 17:53:30 +0900

    コントローラ追加

---
+++ resources/views/tweet/index.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+            content="">
+    <meta http-equiv=”X-UA-Compatible” content=”IE=edge″>
+    <title>つぶやきアプリ</title>
+ </head>
+<body>
+    <h1>つぶやきアプリ</h1>
+    <p>{{$name}}</p>
+</body>
+</html>
